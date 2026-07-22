7e0fc540e56db5487e02031923ba9a4181d1ba4f
karamnezhadgame-dotcom <karamnezhad.game@gmail.com>
2026-07-23 02:31:22 +0330

    Create RetrofitClient.kt
    Number 18

---
+++ app/src/main/java/com/honorassistant/app/data/network/RetrofitClient.kt
@@ -0,0 +1,23 @@
+package com.honorassistant.app.data.network
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "https://api.honorassistant.com/"
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(MockInterceptor())
+        .addInterceptor(okhttp3.logging.HttpLoggingInterceptor().apply {
+            level = okhttp3.logging.HttpLoggingInterceptor.Level.BODY
+        })
+        .build()
+    val api: HeroApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(HeroApiService::class.java)
+    }
+}
