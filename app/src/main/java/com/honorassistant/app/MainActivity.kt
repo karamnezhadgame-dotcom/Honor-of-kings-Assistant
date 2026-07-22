f94c4680ab220dc9032ddc1921138eb093b32bbf
karamnezhadgame-dotcom <karamnezhad.game@gmail.com>
2026-07-23 02:12:26 +0330

    Create MainActivity.kt
    Number 9

---
+++ app/src/main/java/com/honorassistant/app/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.honorassistant.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.honorassistant.app.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = binding.navHostFragment.findNavController()
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.heroListFragment, R.id.insightsFragment, R.id.loreFragment)
+        )
+        binding.bottomNav.setupWithNavController(navController)
+    }
+}
