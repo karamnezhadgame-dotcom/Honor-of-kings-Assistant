5ab6ed05afaf03781f7c231e5872084a0ec90117
karamnezhadgame-dotcom <karamnezhad.game@gmail.com>
2026-07-23 02:20:01 +0330

    Create Item.kt
    Number 12

---
+++ app/src/main/java/com/honorassistant/app/data/models/Item.kt
@@ -0,0 +1,10 @@
+package com.honorassistant.app.data.models
+
+data class Item(
+    val id: String,
+    val name: String,
+    val iconUrl: String,
+    val price: Int,
+    val stats: String,
+    val category: String
+)
